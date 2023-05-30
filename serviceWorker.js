9543297e912766d0ba80e3f55046706063202155
Jeffrey <jowoloko@gmail.com>
2023-05-30 02:17:26 +0200

    add serviceWorker and manifest.json

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const attendanceApp = "attendance-app-v1"
+const assets = [
+  "/",
+  "/login-mobile.html",
+  "/style.css",
+  "/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(attendanceApp).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+});
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
